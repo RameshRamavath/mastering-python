e8128163e58932396fb8aeb6a20d4e24467ca425
RameshRamavath <naik.iitkgp@gmail.com>
2020-03-10 10:49:58 +0530

    Added collections & Array

---
+++ CodingPractice/CheckForSum.py
@@ -0,0 +1,24 @@
+# given an array and sum, check if adding any two elements in Array gives that sum
+# example:: Array(1,3,6,2,8,5), sum = 11 --> Answer: boolean Yes
+
+
+def checkForSum(arr, req_sum):
+
+    # Set to store visited elements
+    lookup_set = set()
+    # check the diff and lookup in collection if we already seen that diff
+    for element in arr:
+        delta = req_sum - element
+        if lookup_set.__contains__(delta):
+            return True
+        else:
+            lookup_set.add(element)
+
+    return False
+
+
+if __name__ == '__main__':
+    arr = [1,3,6,2,8,5]
+    # call our method
+    print checkForSum(arr, 11)  # True
+    print checkForSum(arr, 20)  # False
---
+++ Collections/Arrays.py
@@ -0,0 +1,19 @@
+# In Python we can treat List as CodingPractice but we can't limit List to same data type elements in it
+
+# Example List with diff data types
+
+test_list = [1, 2, "Mahesh", 10.5]
+
+print test_list
+print test_list.__class__
+
+# CodingPractice don't accept diff type of elements in it - hence we have 'array' module in Python
+
+import array as ar
+test_arr = ar.array('i', [1,2,5,7])  # array() takes at most 2 arguments - type & elements
+
+print test_arr
+print test_arr.__class__
+
+for ele in test_arr:
+    print ele
